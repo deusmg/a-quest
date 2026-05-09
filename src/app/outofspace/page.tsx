7995849db839a5a4fa96db1614e8b803250dd4f1
deusmg <laptevap@gmail.com>
2026-05-09 19:04:46 +0300

    edit page

--- src/app/outofspace/page.tsx
+++ src/app/outofspace/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import {Canvas} from "@react-three/fiber";
+
 export default function OutOfSpacePage() {
   return (
-    <main>
-	<h1>Out of Space</h1>
-	<p>Здесь будет космос</p>
+    <main style={{ width: "100vw", height: "100vh", background: "black" }}>
+	<Canvas>
+	  <mesh>
+	   <sphereGeometry args={[0.08, 16, 16]} />
+	   <meshBasicMaterial color="white" />	
+	  </mesh>
+	</Canvas>
     </main> 
  );
 }
